44fb675ffca8281aaa0ec81bb1b65f4e5249ba3f
Dominic Essien <essiendominic1@gmail.com>
2021-02-16 23:44:55 +0100

    refactor viewset to base class w/ similar properties

--- app/pipl/views.py
+++ app/pipl/views.py
@@ -8,33 +8,27 @@ from .renderers import TagRenderer
 from core.models import Tag, Pip
 from pipl import serializers
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
-    """ Manage tags in the database"""
+class BasePiplAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    """Base viewset for userowned piple attributes """
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """ Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+    def perform_create(self, serializer):
+        """Create a new object"""
+        serializer.save(user=self.request.user)
+
+class TagViewSet(BasePiplAttrViewSet):
+    """ Manage tags in the database"""
     renderer_classes = (TagRenderer,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
+    
 
-    def get_queryset(self):
-        """ Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new tag"""
-        serializer.save(user=self.request.user)
-
-class PipViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class PipViewSet(BasePiplAttrViewSet):
     """ Manage the pips in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Pip.objects.all()
     serializer_class = serializers.PipSerializer
-
-    def get_queryset(self):
-        """ Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a new pip"""
-        serializer.save(user=self.request.user)
