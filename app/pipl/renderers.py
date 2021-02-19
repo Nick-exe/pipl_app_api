550a1e6bdf9dadb2fca8051e56d7155f692dc7e0
Dominic Essien <essiendominic1@gmail.com>
2021-02-19 12:38:13 +0100

    Adjust renderer code to match Feature and FeatureCollection

--- app/pipl/renderers.py
+++ app/pipl/renderers.py
@@ -3,8 +3,6 @@ import json
 from rest_framework.response import Response
 
 
-
-
 class TagRenderer(renderers.JSONRenderer):
     charset = 'utf-8'
 
@@ -12,14 +10,28 @@ class TagRenderer(renderers.JSONRenderer):
         response = ''
         if 'Error' in str(data):
             response = json.dumps({'errors': data})
-        else: 
-            response_dict = {
-                'id': '',
-                'coordinates': '',
-                'name': ''
-            }
-            response_dict['id'] = data.get('features')[0]['id']
-            response_dict['coordinates'] = data.get('features')[0]['geometry']['coordinates']
-            response_dict['name'] = data.get('features')[0]['properties']['name']
-            response = json.dumps({'data': response_dict})
+        else:
+            if data['type'] == 'Feature':
+                response_dict = {
+                    'id': '',
+                    'coordinates': '',
+                    'name': ''
+                }
+                response_dict['id'] = data.get('id')
+                response_dict['coordinates'] = data.get('geometry')['coordinates']
+                response_dict['name'] = data.get('properties')['name']
+                response = json.dumps(response_dict)
+            elif data['type'] == 'FeatureCollection':
+                res_list = []
+                for feature in data['features']:
+                    response_dict = {
+                        'id': '',
+                        'coordinates': '',
+                        'name': ''
+                    }
+                    response_dict['id'] = feature['id']
+                    response_dict['coordinates'] = feature['geometry']['coordinates']
+                    response_dict['name'] = feature['properties']['name']
+                    res_list.append(response_dict)
+                    response = json.dumps(res_list)
         return response
